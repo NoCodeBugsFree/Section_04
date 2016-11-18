bbda057c7630d645bc97f282c5a1d473fb3f1b21
SergeevAlexander <arzhanok@mail.ru>
2016-11-18 21:16:49 +0300

    Git Ignore

---
+++ BattleTanK/Source/BattleTanK/BattleTanKGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BattleTanKGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ABattleTanKGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	// setting repo
+	
+	
+};
