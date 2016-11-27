98d01852291067d23b16fb28e29cd5abb78ba5bb
SergeevAlexander <arzhanok@mail.ru>
2016-11-27 18:32:21 +0300

    FVector::CrossProduct(TankForward, AIForwardIntention)

--- BattleTanK/Source/BattleTanK/TankMovementComponent.cpp
+++ BattleTanK/Source/BattleTanK/TankMovementComponent.cpp
@@ -44,9 +44,15 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 	FVector TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
 
 	// Calculate the dot product of two vectors.
-	// A * B = ||A|| ||B|| cos @, where is @ is angle between A and B
+	// A DOT B = ||A|| ||B|| cos @  , where @ is angle between A and B  // DOT IS POINT .  
 	// Y-axis is throw and X-axis is AIForwardIntention
-	float ForwardThrow = FVector::DotProduct(AIForwardIntention, TankForward);
-
+	float ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
 	IntendMoveForward(ForwardThrow);
+
+	// Calculate the cross product of two vectors.
+	// A CROSS B = ||A|| ||B|| sin @  N , where @ is angle between A and B in the plain 
+	// containing them (hence, it is between 0 and 180), ||A|| and ||B|| are the magnitudes
+	// and N is a unit vector perpendicular to the plane
+	FVector RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention);
+	IntendTurnRight(RightThrow.Z); // Z - remember wiki diagram
 }
